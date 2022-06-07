1d6cb63101c886177579dad1335530aed436fa8e
abusayedmdraihan <106584322+abusayedmdraihan@users.noreply.github.com>
2022-06-07 15:25:42 +0530

    Update demo1.js

--- demo1.js
+++ demo1.js
@@ -1,4 +1,4 @@
 module.exports = async function(context, commands) {
-  await commands.navigate('https://www.sitespeed.io');
+  await commands.navigate('https://www.google.com');
   // We will get here when the action is finished since we use await
 }
